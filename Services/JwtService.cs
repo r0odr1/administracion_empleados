4d0975cde7f1386da56f27d3b53849f18f9437c9
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-20 17:40:48 -0500

    Creacion servicio JWT

---
+++ Services/JwtService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EmployeeApi.Services;
+
+public class JwtService
+{
+    private readonly IConfiguration _config;
+    public JwtService(IConfiguration config) => _config = config;
+    public string GenerateToken(string username)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: new[] { new Claim(ClaimTypes.Name, username) },
+            expires: DateTime.UtcNow.AddHours(8),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
