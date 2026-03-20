8ec286bd065b1658fcbf1c8eb4c5398b9352bcc1
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-20 17:28:08 -0500

    Creacion modelo de empleado

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeApi.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Apellido { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Cargo { get; set; } = string.Empty;
+    public DateTime FechaIngreso { get; set; }
+    public bool Estado { get; set; } = true; // true = Estado Activo
+}
